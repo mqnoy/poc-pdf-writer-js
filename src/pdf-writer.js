6cc6e454a1449a25fdcf0aa32d6e9df8e566b34c
mqnoy <qnoy.social@gmail.com>
2025-11-23 14:57:51 +0700

    refactor: pdfWritter support with html content

--- src/pdf-writer.js
+++ src/pdf-writer.js
@@ -1,26 +1,30 @@
 const puppeteer = require("puppeteer");
 
-const run = async () => {
+const run = async (
+  html,
+  options = {
+    pdfOpts: {
+      paperFormat: "A4",
+      margin: {
+        bottom: "10px",
+        top: "10px",
+        left: "10px",
+        right: "10px",
+      },
+    },
+  }
+) => {
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
-  await page.goto("http://google.com", {
-    waitUntil: "networkidle2",
-    timeout: 60000,
-  });
+  await page.setContent(html, { waitUntil: "networkidle0", timeout: 60000 });
 
   const pdfBuffer = await page.pdf({
     printBackground: true,
-    paperFormat: "A4",
-    margin: {
-      bottom: "10px",
-      top: "10px",
-      left: "10px",
-      right: "10px",
-    },
+    ...options.pdfOpts,
   });
 
   await browser.close();
